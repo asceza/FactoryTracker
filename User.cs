bbf6041fe655af582b51f3e84b177b0da0ae46bb
John Asceza <User@WIN-S6T3EKH0CAH>
2024-09-07 10:27:51 +0300

    Добавьте файлы проекта.

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryTracker
+{
+    /// <summary>
+    /// Пользователь, пребывающий в системе
+    /// </summary>
+    public class User
+    {
+        public uint ID { get; set; }
+        public string? FullName { get; set; }
+        public UserRole? Role { get; set; }
+
+        public User(uint id, string? fullName, UserRole? role)
+        {
+            ID = id;
+            FullName = fullName;
+            Role = role;
+        }
+
+    }
+}
---
+++ UserRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryTracker
+{
+    /// <summary>
+    /// Роли пользователя
+    /// </summary>
+    public enum UserRole
+    {
+        None,
+        Brigadier,  // бригадир
+        Master,     // мастер
+        Director,   // директор
+        Controller, // контроллер
+        Logistician,// логист
+    }
+}
